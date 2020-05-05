0b96f768f4ac8a486e68efe805e231f8d7535b60
Kris Chambers <kris.chambers@outlook.com>
2020-05-05 11:46:38 -0400

    Added simple rendering test

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -11,21 +11,44 @@ fn main() {
     let stdout = stdout();
     let mut stdout = stdout.lock().into_raw_mode().unwrap();
 
+    let buffer = [0u64; 32];
+
     let mut count = 0;
 
     write!(stdout, "{}", termion::clear::All).unwrap();
 
     loop {
-        write!(stdout, "{}{}", termion::cursor::Goto(1,1), count).unwrap();
-        write!(stdout, "{}", termion::cursor::Goto(1,2)).unwrap();
-        stdout.flush().unwrap();
+        write!(stdout, "{}{}{}",
+            termion::clear::All,
+            termion::cursor::Goto(1,1),
+            count
+        ).unwrap();
+        
+        write!(stdout, "{}",
+            termion::cursor::Goto(1,2)
+        ).unwrap();
+        
         count += 1;
       
         if let Some(Ok(Event::Key(Key::Char(c)))) = stdin.next() {
             match c {
                 'q' => break,
-                _ => println!("{}", c)
+                _ => { }
             };
         }
+
+        let screen = buffer.iter()
+            .map(|line| format!("{:064b}", line))
+            .collect::<Vec<String>>()
+            .join("\r\n");
+
+        write!(stdout, "{}", screen).unwrap();
+        stdout.flush().unwrap();
+        std::thread::sleep(std::time::Duration::from_millis(10));
   }
+}
+
+#[test]
+fn line_test() {
+    println!("{:06b}", 0);
 }
